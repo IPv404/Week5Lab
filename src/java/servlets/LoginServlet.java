0f1cd625747d8e694e0cb9b0e48b715f911e8c9e
Zeina Mint <mmcka@10.0.0.133>
2023-02-12 23:59:08 -0700

    Servlet Programming Incomplete, still errors and needs review :(

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -6,11 +6,13 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+
 
 /**
  *
@@ -18,69 +20,46 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class LoginServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+ 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //get session
+        HttpSession session = request.getSession();
+        
+        String logout = request.getParameter("logout");
+        String username = (String) session.getAttribute("username");
+        if(logout !=null || username == null ){
+            session.invalidate();
+            session = request.getSession();
+
+            session.invalidate(); 
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            return;
+        }
+        
+        
+        if (username != null ) {
+            session.setAttribute("username", username);
+            getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+            return;
+        }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        HttpSession session = request.getSession();
+          String username = request.getParameter("username");
+          String password = request.getParameter("password");
+          request.setAttribute("username",username);
+          request.setAttribute("password",password);
+           if(password==null||password.equals("")||username==null||username.equals("")){
+               Boolean notFull = true;
+               request.setAttribute("notfull", notFull);
+               getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+               return;
+           }
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
